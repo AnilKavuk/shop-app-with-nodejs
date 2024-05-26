965a4cb9f813b3fda2c72173d547a9ee7a30b671
ANIL KAVUK <36512716+MrKavuk@users.noreply.github.com>
2024-05-26 12:55:16 +0300

    Delete Record

--- routes/products.js
+++ routes/products.js
@@ -22,32 +22,6 @@ const products = [
 ];
 
 router.get("/", async (req, res) => {
-  // const products = await Product.find();
-  // const products = await Product.find({ price: { $eq: 10000 } });
-  // const products = await Product.find({ price: { $ne: 10000 } });
-  // const products = await Product.find({ price: { $gt: 10000 } });
-  // const products = await Product.find({ price: { $gte: 10000 } });
-  // const products = await Product.find({ price: { $lt: 10000 } }); // price < 10000
-  // const products = await Product.find({ price: { $lte: 10000 } }); // price <= 10000
-  // const products = await Product.find({ price: { $in: [10000, 20000] } });
-  // const products = await Product.find({ price: { $nin: [10000, 20000] } });
-  // const products = await Product.find({ price: { $gte: 10000, $lte: 20000 } });
-  // const products = await Product.find({ price: { $gte: 10000, $lte: 20000 }, name: "Samsung" }); // and
-  // const products = await Product.find()
-  //                             .or([
-  //                                 { price: { $gte: 10000} },
-  //                                 {isActive: true }
-  //                             ]); // (price >= 10000 or isActive==true)
-
-  // startwith
-  // const products = await Product.find({ name: /^iphone/ });
-
-  // endwith
-  // const products = await Product.find({ name: /iphone$/ });
-
-  // contains
-  // const products = await Product.find({ name: /.*iphone.*/i });
-
   const products = await Product.find({ isActive: true });
   res.send(products);
 });
@@ -79,37 +53,36 @@ router.post("/", (req, res) => {
   }
 });
 
-router.put("/:id", (req, res) => {
-  const product = products.find((product) => product.id == req.params.id);
-
+router.put("/:id", async (req, res) => {
+  const product = await Product.findById(req.params.id);
   if (!product) {
     res.status(404).send("aradığınız ürün bulunamadı.");
     return;
   }
-
   const { error } = validateProduct(req.body);
-
   if (error) {
     res.status(400).send(error.details[0].message);
     return;
   }
-
   product.name = req.body.name;
   product.price = req.body.price;
-
-  res.send(product);
+  product.description = req.body.description;
+  product.imageUrl = req.body.imageUrl;
+  product.isActive = req.body.isActive;
+  const updatedProduct = await product.save();
+  res.send(updatedProduct);
 });
 
-router.delete("/:id", (req, res) => {
-  const product = products.find((product) => product.id == req.params.id);
+router.delete("/:id", async (req, res) => {
+  let product = null;
+  if (req.params.id.length === 24) {
+    product = await Product.findByIdAndDelete(req.params.id);
+  }
 
   if (!product) {
     res.status(404).send("aradığınız ürün bulunamadı.");
     return;
   }
-
-  const index = products.indexOf(product);
-  products.splice(index, 1);
 
   res.send(product);
 });
