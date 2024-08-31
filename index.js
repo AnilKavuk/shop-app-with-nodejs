8e36fcc03b952fc1d9d5e0bc6d40f87c5e3bfcba
ANIL KAVUK <36512716+MrKavuk@users.noreply.github.com>
2024-08-31 16:34:42 +0300

    injectSpeedInsights implemetion

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require("express");
 const app = express();
 
 const { port } = require("./config");
+const { injectSpeedInsights } = require("@vercel/speed-insights");
 
 require("./startup/logger");
 require("./startup/routes")(app);
 require("./startup/db");
 if (process.env.NODE_ENV === "production") {
   require("./startup/production")(app);
+  injectSpeedInsights();
 }
 
 app.listen(port, () => {
