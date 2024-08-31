c40a0ce7c31dfba2f2bf9d9281d493f55d64668e
ANIL KAVUK <36512716+MrKavuk@users.noreply.github.com>
2024-08-31 15:20:59 +0300

    test vercel

--- config.js
+++ config.js
@@ -1,21 +1,23 @@
 const dotEnv = require("dotenv");
 const result = dotEnv.config();
 
-if (result.error && process.env.NODE_ENV === "development") {
-  throw result.error;
+if (result.error) {
+  console.warn(
+    ".env file not found, falling back to Vercel environment variables"
+  );
 }
 
-let db = null;
-if (process.env.NODE_ENV === "development") {
-  db = process.env.DB_DEV_URL;
-} else if (process.env.NODE_ENV === "production") {
-  db = process.env.DB_PROD_URL;
-}
+const db =
+  process.env.NODE_ENV === "development"
+    ? process.env.DB_DEV_URL
+    : process.env.DB_PROD_URL;
 
 const port = process.env.PORT ?? 3000;
-
 const saltRounds = process.env.SALT_ROUNDS;
-
 const secretKey = process.env.SECRET_KEY;
+
+if (!db || !port || !saltRounds || !secretKey) {
+  throw new Error("Required environment variables are missing.");
+}
 
 module.exports = { db, port, saltRounds, secretKey };
